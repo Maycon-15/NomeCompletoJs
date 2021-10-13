2c831c04a2af6d5a03df35807e0147d08b298a8e
Ana-Costa12 <staxgirl01@gmail.com>
2021-10-13 20:58:33 -0300

    Finalização

---
+++ NomeCompleto.js
@@ -0,0 +1,8 @@
+let Nome = prompt("Digite seu nome:");
+let Sobrenome = prompt("Digite seu sobrenome:");
+
+let NomeCompleto = `Seu nome é ${Nome} ${Sobrenome}.`;
+alert(NomeCompleto);
+
+let NomeCatalogo = `Seu nome catalogo é ${Sobrenome.toUpperCase()}, ${Nome}`;
+alert(NomeCatalogo);
